3062d61075a035d72769f464229f163f54229594
Jose Duarte <jduarte0912@gmail.com>
2025-09-27 21:16:22 -0700

    settings and then user testing then done

--- pwa/src/components/settings/settingsContainer.svelte
+++ pwa/src/components/settings/settingsContainer.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div
-  class="absolute top-[60px] bottom-[75px] min-h-[calc(100vh_-_115px)] w-full p-10"
+  class="absolute top-[60px] bottom-[75px] min-h-[calc(100vh_-_115px)] w-full p-10 flex flex-col gap-5"
 >
+  <Card.Root></Card.Root>
+  <Card.Root></Card.Root>
   <Card.Root></Card.Root>
 </div>
