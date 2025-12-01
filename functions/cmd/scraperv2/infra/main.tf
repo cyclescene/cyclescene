cc34d50b0c762e4ca647d391db706e5f2b68525e
Jose Duarte <jduarte0912@gmail.com>
2025-12-01 13:43:32 -0800

    fix: update Cloud Scheduler API endpoints for Cloud Run v2 Jobs across all services
    Replace deprecated namespaces-based API endpoints with correct projects/locations format
    for Cloud Run v2 Jobs. This fixes Cloud Scheduler job failures for:
    - scraperv2 (PDX scraper)
    - db-backups (Turso database backups)
    - token-cleaner (Submission token cleanup)
    
    The old v1 API format used 'namespaces' which is incompatible with Cloud Run v2 Jobs.
    The new v2 API format correctly uses 'projects/{project}/locations/{region}/jobs/{job}:run'.

--- functions/cmd/scraperv2/infra/main.tf
+++ functions/cmd/scraperv2/infra/main.tf
@@ -101,7 +101,7 @@ module "scraper_schedule" {
   time_zone   = var.scraper_timezone
 
   http_target = {
-    uri         = "https://${var.region}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.project_id}/jobs/${module.scraper_job.job_name}:run"
+    uri         = "https://${var.region}-run.googleapis.com/apis/run.googleapis.com/v1/projects/${var.project_id}/locations/${var.region}/jobs/${module.scraper_job.job_name}:run"
     http_method = "POST"
     oidc_token = {
       service_account_email = module.scheduler_service_account.email
