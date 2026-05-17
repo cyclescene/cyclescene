cabc04b750ecafc8757d7152547a7f362a47db5b
Jose Duarte <jduarte0912@gmail.com>
2026-05-17 12:28:26 -0700

    fixed image size slider now it actually does something.

--- frontends/form/src/lib/components/group-form/CustomMarkerBuilder.svelte
+++ frontends/form/src/lib/components/group-form/CustomMarkerBuilder.svelte
@@ -44,16 +44,14 @@
   const SVG_PATH =
     "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z";
 
-  // Re-render when image preview loads
+  // Re-render when the uploaded image, marker color, or image zoom changes.
   $effect(() => {
-    if (imagePreview && canvasRef) {
-      renderCanvas();
-    }
-  });
+    const preview = imagePreview;
+    const canvas = canvasRef;
+    const color = markerColor;
+    const zoom = imageScale;
 
-  // Re-render when marker color changes while an uploaded image is previewed.
-  $effect(() => {
-    if ((markerColor || imageScale) && imagePreview && canvasRef) {
+    if (preview && canvas && color && zoom) {
       renderCanvas();
     }
   });
